0b97cb5ac42bca80d098795e2b3aa036a538a5a0
Ali Alsayed <a_alsayed87@hotmail.com>
2019-12-10 09:00:06 +0000

    Automatically backed up by Learn

--- components/Row.js
+++ components/Row.js
@@ -12,8 +12,8 @@ export default class Row extends React.Component {
             <Cell 
               key={i} 
               cell={value}
-              onChange={this.props.onChange.bind(null, columnIndex)}
-              onFocus={this.props.onFocus.bind(null, columnIndex)} 
+              onChange={this.props.onChange.bind(null, i)}
+              onFocus={this.props.onFocus.bind(null, i)} 
             />
           )
         }
