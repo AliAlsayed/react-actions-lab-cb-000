8f878879b75342b487c6218596093ed87bdc5cbf
Ali Alsayed <a_alsayed87@hotmail.com>
2019-12-10 08:19:01 +0000

    Automatically backed up by Learn

--- components/Table.js
+++ components/Table.js
@@ -6,8 +6,15 @@ import Row from './Row';
 export default class Table extends React.Component {
   render () {
     return (
+      const table = this.props.table
       <table className='table'>
-        <tbody></tbody>
+        <tbody>
+          {
+            table.map((value, index) =>{
+              <Cell />
+            })
+          }
+        </tbody>
       </table>
     )
   }
