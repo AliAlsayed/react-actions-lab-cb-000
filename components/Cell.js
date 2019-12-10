9ed477eccd55bff311d7de1149a96367cc888b74
Ali Alsayed <a_alsayed87@hotmail.com>
2019-12-10 09:01:08 +0000

    Automatically backed up by Learn

--- components/Cell.js
+++ components/Cell.js
@@ -5,7 +5,13 @@ import React from 'react';
 export default class Cell extends React.Component {
   render () {
     return (
-      <td className='cell'><input></input></td>
+      <td className='cell'>
+        <input
+        value={this.props.cell}
+        onChange={this.props.onChange}
+        onFocus={this.props.onFocus}
+        />
+      </td>
     )
 
   }
