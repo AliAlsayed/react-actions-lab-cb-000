582454e0f7ed969c378bbea84c0b4b9f790fa74b
Ali Alsayed <a_alsayed87@hotmail.com>
2019-12-10 08:48:33 +0000

    Automatically backed up by Learn

--- components/Row.js
+++ components/Row.js
@@ -6,7 +6,13 @@ import Cell from './Cell';
 export default class Row extends React.Component {
   render () {
     return (
-      <tr className='row'></tr>
+      <tr className='row'>
+        {
+          this.props.row.map((value, i) =>
+            <Cell key={i} cell={value} />
+          )
+        }
+      </tr>
     )
   }
 }
